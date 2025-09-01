b960e243d88ab8e930b269b741d7dd47d8657a5e
dipakkhandare203-tech <dipakkhandare203@gmail.com>
2025-09-01 17:31:59 +0530

    New files uploaded

---
+++ AdditionOfThreeNumber.java
@@ -0,0 +1,20 @@
+//A simple program to add three number.
+
+public class Main{
+    public static void main(String[] args) {
+      int  num1 = 25;
+      int  num2 = 54;
+      int  num3 = 32;
+
+      int sum = num1+num2+num3;
+
+      System.out.println("Final answer is "+sum);
+
+
+    }
+}
+
+//Output
+
+/* 
+Final answer is 11*/
